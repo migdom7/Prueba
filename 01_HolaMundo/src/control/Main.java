84718a189a37e9ccbcbeee85d9d2608f27117e79
Raul1551 <80522684+Raul1551@users.noreply.github.com>
2021-03-15 18:46:48 +0100

    Update Main.java

--- 01_HolaMundo/src/control/Main.java
+++ 01_HolaMundo/src/control/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Holadd MundEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRro");
+		System.out.println("Holadd MundEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRro");
 
 	}
 
